b207bbe1c5a9ffea6d795ce362c17ff2417bee47
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-12 21:34:21 +0100

    Update Events

--- app-web/src/main/kotlin/com/scurab/uitor/web/Events.kt
+++ app-web/src/main/kotlin/com/scurab/uitor/web/Events.kt
@@ -2,6 +2,7 @@ package com.scurab.uitor.web
 
 import org.w3c.dom.events.EventTarget
 import org.w3c.dom.events.MouseEvent
+import org.w3c.dom.events.WheelEvent
 
 enum class Events {
     click,
@@ -13,10 +14,20 @@ enum class Events {
     mouseup,
     mousewheel,
     wheel,
+    keydown,
 }
+
+fun EventTarget.addMouseClickListener(callback: (MouseEvent) -> Unit) =
+    addEventListener(Events.click.name, { event -> callback(event as MouseEvent) })
 
 fun EventTarget.addMouseMoveListener(callback: (MouseEvent) -> Unit) =
     addEventListener(Events.mousemove.name, { event -> callback(event as MouseEvent) })
+
+fun EventTarget.addMouseWheelListener(callback: (WheelEvent) -> Unit) =
+    addEventListener(Events.mousewheel.name, { event -> callback(event as WheelEvent) })
+
+fun EventTarget.addMouseOutListener(callback: (MouseEvent) -> Unit) =
+    addEventListener(Events.mouseout.name, { event -> callback(event as MouseEvent) })
 
 fun EventTarget.addClickListener(callback: (MouseEvent) -> Unit) =
     addEventListener(Events.click.name, { event -> callback(event as MouseEvent) })
