047d047c9d39b7ea6643222ab3727eaf12912a44
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-19 16:52:31 +0100

    Add ext function for map k/v mapping

--- app-web/src/main/kotlin/com/scurab/uitor/web/model/ClientConfig.kt
+++ app-web/src/main/kotlin/com/scurab/uitor/web/model/ClientConfig.kt
@@ -3,16 +3,20 @@ package com.scurab.uitor.web.model
 import com.scurab.uitor.common.model.ConfigFields
 import com.scurab.uitor.common.render.Color
 import com.scurab.uitor.common.render.toColor
+import com.scurab.uitor.common.util.map
 import com.scurab.uitor.web.util.getMap
 import com.scurab.uitor.web.util.jsonField
 import kotlin.js.Json
 
 class ClientConfig(private val json: Json) {
     val selectionColor by jsonField<String>(json, ConfigFields.SelectionColor)
-    val propertyHighlights = convertPropertyHighlights(json.getMap(ConfigFields.PropertyHighlights))
+    val propertyHighlights =
+        json.getMap(ConfigFields.PropertyHighlights)
+            .map({ it.toRegex() }, { it.toString().toColor() })
     val serverVersion by jsonField<Int>(json, ConfigFields.ServerVersion)
     val device = json.getMap(ConfigFields.Device)
     val typeHighlights = json.getMap(ConfigFields.TypeHighlights)
+        .map({ it }, { it.toString().toColor() })
     val groovy by jsonField<Boolean>(json, ConfigFields.Groovy)
     val pointerIgnoreIds by jsonField<IntArray>(json, ConfigFields.PointerIgnoreIds)
 
--- common/src/commonMain/kotlin/com/scurab/uitor/common/util/Collections.kt
+++ common/src/commonMain/kotlin/com/scurab/uitor/common/util/Collections.kt
@@ -3,3 +3,18 @@ package com.scurab.uitor.common.util
 inline fun <T> Collection<T>.forEachReversed(action: (T) -> Unit): Unit {
     for (i in size - 1 downTo 0) action(this.elementAt(i))
 }
+
+fun <KIN, VIN, KOUT, VOUT> Map<KIN, VIN>.map(keyMapper: (KIN) -> KOUT, valueMapper: (VIN) -> VOUT): Map<KOUT, VOUT> {
+    return mapTo(keyMapper, valueMapper, mutableMapOf())
+}
+
+fun <KIN, VIN, KOUT, VOUT> Map<KIN, VIN>.mapTo(
+    keyMapper: (KIN) -> KOUT,
+    valueMapper: (VIN) -> VOUT,
+    to: MutableMap<KOUT, VOUT>
+): Map<KOUT, VOUT> {
+    this.forEach { (k, v) ->
+        to[keyMapper(k)] = valueMapper(v)
+    }
+    return to
+}
