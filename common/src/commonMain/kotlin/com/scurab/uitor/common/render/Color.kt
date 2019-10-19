80b1d07c7cd6bc11dc85be49407600d903d8d0d8
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-19 15:31:55 +0100

    Update Color parsing

--- common/src/commonMain/kotlin/com/scurab/uitor/common/render/Color.kt
+++ common/src/commonMain/kotlin/com/scurab/uitor/common/render/Color.kt
@@ -1,5 +1,7 @@
 package com.scurab.uitor.common.render
 
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.roundToInt
 
 @Suppress("MemberVisibilityCanBePrivate")
@@ -9,19 +11,59 @@ data class Color(val value: Int) {
     val green: Int = (value ushr 8 and 0xFF)
     val blue: Int = (value and 0xFF)
 
+    private val alphaAs2Decimals: String
+        get() =
+            ((alpha * 100).roundToInt() / 100.0).toString()
+
     val htmlRGB = "#${red.hex}${green.hex}${blue.hex}"
     val htmlARGB = "#${(alpha * 255).roundToInt().hex}${red.hex}${green.hex}${blue.hex}"
-    val htmlRGBA = "rgba($red, $green, $blue, $alpha)"
+    val htmlRGBA = "rgba($red, $green, $blue, $alphaAs2Decimals)"
+
+    fun withAlpha(alpha: Double): Color {
+        val v = value and 0x00FFFFFF//remove alpha
+        val alpha = (min(1.0, max(0.0, alpha)) * 255.0).roundToInt()
+        return Color(v or (alpha shl 24))
+    }
 }
 
+private val CSS_COLOR_REGEX = "rgba?\\((.*)\\)".toRegex()
 fun String.toColor(): Color {
     var temp = substringAfter("#")
-    when {
-        temp.length == 3 -> temp = "F$temp".map { "$it$it" }.joinToString("")
-        temp.length == 4 -> temp = temp.map { "$it$it" }.joinToString("")
-        temp.length == 6 -> temp = "FF$temp"
-        temp.length == 8 -> {}//ok nothing
-        else -> throw IllegalArgumentException("Invalid format of string color:'$this'")
+    val result = if (temp.startsWith("rgb")) {
+        parseFromCss()
+    } else {
+        when {
+            temp.startsWith("rgb") -> parseFromCss()
+            temp.length == 3 -> temp = "F$temp".map { "$it$it" }.joinToString("")
+            temp.length == 4 -> temp = temp.map { "$it$it" }.joinToString("")
+            temp.length == 6 -> temp = "FF$temp"
+            temp.length == 8 -> {
+            }//ok nothing
+        }
+        return Color(temp.toLong(16).toInt())
     }
-    return Color(temp.toLong(16).toInt())
+    return result ?: throw IllegalArgumentException("Invalid format of string color:'$this'")
+}
+
+//rgba(255, 0, 255, 0.15)
+//rgb(255, 0, 255)
+private fun String.parseFromCss(): Color? {
+    return CSS_COLOR_REGEX.find(this)?.groupValues
+        ?.takeIf { it.size == 2 }
+        ?.get(1)
+        ?.split(",")
+        ?.map { it.trim() }
+        ?.takeIf { it.size in (3..4) }
+        ?.let {
+            val r = it[0].toInt()
+            val g = it[1].toInt()
+            val b = it[2].toInt()
+            val a = if (it.size == 4) (it[3].toFloat() * 255).roundToInt() else 255
+            Color(
+                (a and 0xFF shl 24) or
+                        (r and 0xFF shl 16) or
+                        (g and 0xFF shl 8) or
+                        (b and 0xFF shl 0)
+            )
+        }
 }
--- common/src/jvmTest/kotlin/com/scurab/uitor/common/render/ColorTest.kt
+++ common/src/jvmTest/kotlin/com/scurab/uitor/common/render/ColorTest.kt
@@ -6,22 +6,29 @@ import junitparams.Parameters
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 @RunWith(JUnitParamsRunner::class)
-internal class ColorTest {
+class ColorTest {
 
     @Test
-    @Parameters(
-        value = [
-            "000, #FF000000",
-            "F000, #FF000000",
-            "111, #FF111111",
-            "F123, #FF112233",
-            "112233, #FF112233",
-            "AA112233, #AA112233"
-        ]
-    )
+    @Parameters(method = "testValues")
     fun testColor(value: String, expected: String) {
         assertEquals(expected, value.toColor().htmlARGB)
+    }
+
+    companion object {
+        @JvmStatic
+        fun testValues(): Array<Array<String>> {
+            return arrayOf(
+                arrayOf("000", "#FF000000"),
+                arrayOf("000", "#FF000000"),
+                arrayOf("F000", "#FF000000"),
+                arrayOf("111", "#FF111111"),
+                arrayOf("F123", "#FF112233"),
+                arrayOf("112233", "#FF112233"),
+                arrayOf("AA112233", "#AA112233"),
+                arrayOf("rgba(255, 0, 255, 0.5)", "#80FF00FF"),
+                arrayOf("rgb(200, 127, 0)", "#FFC87F00")
+            )
+        }
     }
 }
