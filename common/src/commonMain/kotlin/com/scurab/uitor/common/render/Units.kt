51146adda59c7e62ec550a343b651e92787cd852
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-26 15:22:56 +0100

    Update units

--- common/src/commonMain/kotlin/com/scurab/uitor/common/render/Units.kt
+++ common/src/commonMain/kotlin/com/scurab/uitor/common/render/Units.kt
@@ -1,5 +1,10 @@
+@file:Suppress("NAME_SHADOWING")
+
 package com.scurab.uitor.common.render
 
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.pow
 import kotlin.math.roundToInt
 
 val Int.px get() = "${this}px"
@@ -14,4 +19,11 @@ val Int.hex
         }.toUpperCase()
 
 
-fun Double.relativeToScale(scale:Double) = this / scale
+fun Double.relativeToScale(scale: Double) = this / scale
+
+val Number.r2 get() = roundWithDecimalPlaces(2)
+fun Number.roundWithDecimalPlaces(decimals: Int = 2): String {
+    val decimals = max(1, min(decimals, 8))
+    val d = 10.0.pow(decimals)
+    return ((this.toDouble() * d).roundToInt() / d).toString()
+}
