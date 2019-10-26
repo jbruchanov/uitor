65364828bd2208da71e6050b781419406a3c3c90
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-26 15:23:19 +0100

    Optimize imports

--- app-web/src/main/kotlin/com/scurab/uitor/web/tree/LayoutRenderDelegate.kt
+++ app-web/src/main/kotlin/com/scurab/uitor/web/tree/LayoutRenderDelegate.kt
@@ -3,6 +3,7 @@ package com.scurab.uitor.web.tree
 import com.scurab.uitor.web.model.ViewNode
 import d3.Link
 import d3.Node
+import d3.translate
 import kotlin.math.max
 
 interface LayoutRenderDelegate {
@@ -34,10 +35,10 @@ data class LayoutResult(
  * Layout delegate to have a horizontally-growing tree
  */
 class HorizontalDelegate(private val denseFirstNColumns: Int = 0) : LayoutRenderDelegate {
-    override fun translateNode(d: Node<*>): String = d3.translate(d.y, d.x)
+    override fun translateNode(d: Node<*>): String = translate(d.y, d.x)
     override fun drawingPath(): Link = d3.linkHorizontal().x { it.y }.y { it.x }
     override fun graphTranslation(config: TreeConfig, root: Node<*>, x0: Double): String {
-        return d3.translate(
+        return translate(
             if (config.viewGroupAnchorEnd) config.nodeOffsetX else config.nodeOffsetY / 2,
             root.dx - x0
         )
@@ -78,10 +79,10 @@ class HorizontalDelegate(private val denseFirstNColumns: Int = 0) : LayoutRender
  * Layout delegate to have a vertically-growing tree
  */
 class VerticalDelegate : LayoutRenderDelegate {
-    override fun translateNode(d: Node<*>): String = d3.translate(d.x, d.y)
+    override fun translateNode(d: Node<*>): String = translate(d.x, d.y)
     override fun drawingPath(): Link = d3.linkVertical().x { it.x }.y { it.y }
     override fun graphTranslation(config: TreeConfig, root: Node<*>, x0: Double): String {
-        return d3.translate(
+        return translate(
             root.dx - x0,
             if (config.viewGroupAnchorEnd) config.nodeOffsetX else config.nodeOffsetY / 2
         )
