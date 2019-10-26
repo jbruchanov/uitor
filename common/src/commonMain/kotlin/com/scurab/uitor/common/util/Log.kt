0e5750beed53c2f264914b7f9cf0f451ba836f88
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-26 19:04:09 +0100

    Update log

--- common/src/commonMain/kotlin/com/scurab/uitor/common/util/Log.kt
+++ common/src/commonMain/kotlin/com/scurab/uitor/common/util/Log.kt
@@ -1,6 +1,10 @@
 package com.scurab.uitor.common.util
 
-const val DEBUG = true
+const val DEBUG = 3
+const val VERBOSE = 2
+const val INFO = 1
+const val OFF = 0
+const val LOGGING_MODE = VERBOSE
 
 expect object Log {
     fun dlog(msg: String)
@@ -12,13 +16,23 @@ inline fun dlog(msg: () -> String) {
 }
 
 inline fun dlog(tag: String = "", msg: () -> String) {
-    if (DEBUG) {
+    if (LOGGING_MODE >= DEBUG) {
+        println("[$tag]:${msg()}")
+    }
+}
+
+inline fun vlog(tag: String = "", msg: () -> String) {
+    if (LOGGING_MODE >= VERBOSE) {
+        println("[$tag]:${msg()}")
+    }
+}
+
+inline fun ilog(tag: String = "", msg: () -> String) {
+    if (LOGGING_MODE >= INFO) {
         println("[$tag]:${msg()}")
     }
 }
 
 inline fun elog(tag: String = "", msg: () -> String) {
-    if (DEBUG) {
-        println("[$tag]:${msg()}")
-    }
+    println("[$tag]:${msg()}")
 }
