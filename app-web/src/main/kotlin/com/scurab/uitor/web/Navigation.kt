16bcdf5ec7b37ae0a4ad37a5b858ca2daad3ccd7
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-27 16:22:53 +0000

    Add basic common navigation implementation

---
+++ app-web/src/main/kotlin/com/scurab/uitor/web/Navigation.kt
@@ -0,0 +1,42 @@
+package com.scurab.uitor.web
+
+import App
+import com.scurab.uitor.common.util.ilog
+import com.scurab.uitor.web.util.HashToken
+import com.scurab.uitor.web.util.requireElementById
+import org.w3c.dom.HashChangeEvent
+import org.w3c.dom.events.Event
+import kotlin.browser.document
+import kotlin.browser.window
+
+object Navigation {
+    private val TAG = "Navigation"
+    private val rootElement = document.requireElementById("root")
+    private var currentPage: Page? = null
+
+    init {
+        window.addEventListener("hashchange", { e: Event ->
+            val ev = e as HashChangeEvent
+            val from = HashToken(ev.oldURL)
+            val to = HashToken(ev.newURL)
+            ilog(TAG) { "HashChanged from:'${from.pageId}' to:'${to.pageId}'" }
+            when {
+                currentPage?.id == to.pageId -> currentPage?.onHashTokenChanged(from, to)
+                else -> App.openPageBaseOnUrl()
+            }
+        })
+    }
+
+    fun open(page: Page) {
+        open(page, true)
+    }
+
+    fun open(page: Page, pushState: Boolean) {
+        currentPage?.detach()
+        if (pushState) {
+            window.history.pushState(page.id, page.id, "#${page.id}")
+        }
+        currentPage = page
+        page.attachTo(rootElement)
+    }
+}
