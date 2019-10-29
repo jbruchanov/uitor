5cd1f052e09ccc16e2f0412634438c8f0aa993b8
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-29 20:28:27 +0000

    Add iae shortcut for IllegalArgumentException

--- common/src/commonMain/kotlin/com/scurab/uitor/common/util/Exceptions.kt
+++ common/src/commonMain/kotlin/com/scurab/uitor/common/util/Exceptions.kt
@@ -2,6 +2,7 @@ package com.scurab.uitor.common.util
 
 fun npe(msg: String): Nothing = throw NullPointerException(msg)
 fun ise(msg: String): Nothing = throw IllegalStateException(msg)
+fun iae(msg: String): Nothing = throw IllegalArgumentException(msg)
 
 val <T> T?.ref: T
     get() {
