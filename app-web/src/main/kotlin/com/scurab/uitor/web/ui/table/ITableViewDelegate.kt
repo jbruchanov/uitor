61784ae0c3e80a5f856daba37b0667d2446e4686
Jiri Bruchanov <jbruchanov@gmail.com>
2019-10-30 23:02:35 +0000

    Clean

--- app-web/src/main/kotlin/com/scurab/uitor/web/ui/table/ITableViewDelegate.kt
+++ app-web/src/main/kotlin/com/scurab/uitor/web/ui/table/ITableViewDelegate.kt
@@ -1,5 +1,8 @@
 package com.scurab.uitor.web.ui.table
 
+import com.scurab.uitor.web.model.ClientConfig
+import com.scurab.uitor.web.ui.ViewPropertiesTableViewComponents
+
 interface ITableViewDelegate<T> {
     var data: ITableData<T>
     var render: ITableViewRenderer<T>
@@ -12,4 +15,11 @@ open class TableViewDelegate<T>(
     override var render: ITableViewRenderer<T>,
     override val enableFilter: Boolean = true,
     override val filterDebounce: Int = 200
-) : ITableViewDelegate<T>
+) : ITableViewDelegate<T> {
+    companion object {
+        fun default(clientConfig: ClientConfig) = TableViewDelegate(
+            data = TableData.empty(),
+            render = ViewPropertiesTableViewComponents.columnRenderer(clientConfig)
+        )
+    }
+}
