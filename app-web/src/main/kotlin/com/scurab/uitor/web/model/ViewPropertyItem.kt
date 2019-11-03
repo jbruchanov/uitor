7b4aa9f9ba84e3851f15af673fab1bff1f9e9ca3
Jiri Bruchanov <jbruchanov@gmail.com>
2019-11-03 23:41:11 +0000

    Handle nicely string in Context

--- app-web/src/main/kotlin/com/scurab/uitor/web/model/ViewPropertyItem.kt
+++ app-web/src/main/kotlin/com/scurab/uitor/web/model/ViewPropertyItem.kt
@@ -8,17 +8,27 @@ class ViewPropertyItem(private val json: Json) : IResourceItem {
     override val context: String? = null
     val name = json["Name"]?.toString()
     override val dataType = json["DataType"] as String?
-    val properties = json.getMap("Context")
+    val properties = json.getContext()
 
     override fun dataString(): String {
         return json["Data"] as? String ?: ise("Missing field 'Data'")
     }
 
     override fun dataArrayResourcesItems(): List<IResourceItem> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override fun dataArrayPrimitives(): Array<Any> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
+    }
+
+    private fun Json.getContext(): Map<String, Any?> {
+        val any = this["Context"]
+        //null object, TODO handle this in better way on server
+        return if (any is String) {
+            emptyMap()
+        } else {
+            getMap("Context")
+        }
     }
 }
