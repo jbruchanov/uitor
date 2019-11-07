5b828095ae7648fc7dfab560d5b5f60e55a9a4bf
Jiri Bruchanov <jbruchanov@gmail.com>
2019-11-07 22:13:47 +0000

    Update gradle script to able create minified single app artifact

--- app-web/builddev.gradle.kts
+++ app-web/builddev.gradle.kts
@@ -1,56 +1,25 @@
+val kotlinDceOutputDir = file("${project.buildDir}/kotlin-js-min/main")
+val artifactOutputDir = file("${project.buildDir}/out")
+
 /**
  * Task to go through all javascript files generated in DCE
  * And order them to load them based on dependencies between them and update the index_template.html
  */
-val createIndexHtmlTask = task("_createIndexHtml") {
-    group = "build"
+val createDevIndexHtmlTask = task("createDevIndexHtml") {
+    group = "custom build"
     doLast {
-        val kotlinDceOutputDir = file("${project.buildDir}/kotlin-js-min/main")
         val indexHtmlTemplate = file("index_template.html")
 
-        val depRegEx = "require\\('(\\S*)'\\)".toRegex()
-        val deps = mutableMapOf<File, MutableSet<String>>()
-        val baseDir = file(kotlinDceOutputDir)
-        if (!baseDir.exists()) {
-            throw StopExecutionException("$baseDir doesn't exist!")
-        }
-
-        fileTree(baseDir).toList()
-            .filter { it.name.endsWith(".js") }
-            .forEach { f ->
-                val content = f.readText().substringBefore("}")
-                val list = mutableSetOf<String>()
-                deps[f] = list
-                depRegEx
-                    .findAll(content)
-                    .toList()
-                    .takeIf { it.isNotEmpty() }
-                    ?.forEach {
-                        list.add(it.groupValues[1])
-                    }
-            }
-
-        val depsOrdered = mutableListOf<File>()
-        while (deps.isNotEmpty()) {
-            val (file, fileDeps) = deps.entries.first {
-                it.value.isEmpty()
-            }
-
-            val moduleName = file.nameWithoutExtension
-            depsOrdered.add(file)
-            deps.remove(file)
-            deps.forEach { (_, d) -> d.remove(moduleName) }
-        }
-
+        val depsOrdered = getOrderedDeps()
         val exclude = "uitor-app-web.js"
         var text = indexHtmlTemplate.readText()
         text = text.replace("<!--%SCRIPTS%-->",
             depsOrdered
                 .filter { it.name != exclude }
                 .joinToString("\n") {
-                val n = it.relativeTo(project.projectDir).toString().replace("\\", "/")
-                "<script src=\"$n\"></script>"
-            }
+                    val n = it.relativeTo(project.projectDir).toString().replace("\\", "/")
+                    "<script src=\"$n\"></script>"
+                }
         )
         val app = depsOrdered.last().relativeTo(project.projectDir).toString().replace("\\", "/")
         text += """
@@ -69,4 +38,103 @@ val createIndexHtmlTask = task("_createIndexHtml") {
     }
 }
 
-createIndexHtmlTask.dependsOn("runDceKotlin")
+createDevIndexHtmlTask.dependsOn("runDceKotlin")
+
+val generateSingleArtifact = task("createSingleArtifact") {
+    group = "custom build"
+    doLast {
+        artifactOutputDir.mkdirs()
+        val outputFile = File(artifactOutputDir, "release.js")
+        if (outputFile.exists()) {
+            check(outputFile.delete()) { "Unable to delete release file:${outputFile}" }
+        }
+
+        getOrderedDeps().forEach { f ->
+            println(f.absolutePath)
+            outputFile.appendText(f.readText())
+        }
+    }
+}
+
+fun getOrderedDeps(): List<File> {
+    val depRegEx = "require\\('(\\S*)'\\)".toRegex()
+    val deps = mutableMapOf<File, MutableSet<String>>()
+    val baseDir = file(kotlinDceOutputDir)
+    if (!baseDir.exists()) {
+        throw StopExecutionException("$baseDir doesn't exist!")
+    }
+
+    fileTree(baseDir).toList()
+        .filter { it.name.endsWith(".js") }
+        .forEach { f ->
+            val content = f.readText().substringBefore("}")
+            val list = mutableSetOf<String>()
+            deps[f] = list
+            depRegEx
+                .findAll(content)
+                .toList()
+                .takeIf { it.isNotEmpty() }
+                ?.forEach {
+                    list.add(it.groupValues[1])
+                }
+        }
+
+    val depsOrdered = mutableListOf<File>()
+    while (deps.isNotEmpty()) {
+        val (file, fileDeps) = deps.entries.first {
+            it.value.isEmpty()
+        }
+
+        val moduleName = file.nameWithoutExtension
+        depsOrdered.add(file)
+        deps.remove(file)
+        deps.forEach { (_, d) -> d.remove(moduleName) }
+    }
+    return depsOrdered
+}
+generateSingleArtifact.dependsOn(createDevIndexHtmlTask)
+
+val installNpmTask = tasks.register<Exec>("installNpm") {
+    group = "custom build"
+    workingDir = project.projectDir
+    val cmd = "cmd /c npm install"
+    commandLine = cmd.split(" ")
+}
+
+val uglifyjsReleaseArtifactTask = tasks.create<Exec>("uglifyjsReleaseArtifact") {
+    group = "custom build"
+    workingDir = project.projectDir
+    val outputFile = File(artifactOutputDir, "release.js")
+    val outputMinFile = File(artifactOutputDir, "release.min.js")
+    val cmd = "uglifyjs -m -c -o \"$outputMinFile\" \"$outputFile\""
+    commandLine = listOf("cmd", "/c") + cmd.split(" ")
+}
+uglifyjsReleaseArtifactTask.dependsOn(installNpmTask, generateSingleArtifact)
+
+
+//release html
+val createReleaseIndexHtmlTask = task("createReleaseIndexHtml") {
+    group = "custom build"
+    doLast {
+        val indexHtmlTemplate = file("index_template.html")
+        val outputMinFile = File(artifactOutputDir, "release.min.js")
+        var text = indexHtmlTemplate.readText()
+        text = text.replace("<!--%SCRIPTS%-->", "")
+        val app = outputMinFile.relativeTo(project.projectDir).toString().replace("\\", "/")
+        text += """
+            <script>
+                window.onload = function() {
+                    let script = document.createElement('script');
+                    script.src = "$app";
+                    document.head.appendChild(script)
+                };
+            </script>
+        """.trimIndent()
+        file("index.html").apply {
+            delete()
+            writeText(text)
+        }
+    }
+}
+
+createReleaseIndexHtmlTask.dependsOn(uglifyjsReleaseArtifactTask)
