5b831ee074bbfb85200ce0bad4994f860d7c8826
Jiri Bruchanov <jbruchanov@gmail.com>
2022-07-13 21:42:20 +0100

    Update deps versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,7 +3,7 @@ object Versions {
     const val kotlinHtmlJs = "0.7.5"
     const val kotlinCoroutinesCommon = "1.6.3"
     const val kotlinCoroutines = "1.6.3"
-    const val ktor = "1.5.0"
+    const val ktor = "1.6.3"
 }
 
 object Dependencies {
